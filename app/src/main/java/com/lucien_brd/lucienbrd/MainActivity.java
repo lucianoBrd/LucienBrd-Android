96c418adcbffa38f2534b25bc0abbedb9b3cda4e
lucianoBrd <lucien.burdet@gmail.com>
2019-01-13 22:48:13 +0100

    first commit

---
+++ app/src/main/java/com/lucien_brd/lucienbrd/MainActivity.java
@@ -0,0 +1,37 @@
+package com.lucien_brd.lucienbrd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        // Stop local links and redirects from opening in browser instead of WebView
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+
+        mWebView.loadUrl("https://lucien-brd.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
